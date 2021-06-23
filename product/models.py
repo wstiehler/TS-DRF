02b072d792dde0957f0aa16df7dc9a5f50aa421b
william villani stiehler <william.stiehler@olist.com>
2021-06-23 18:39:49 -0300

    Update model increment validate_filds with exception error

--- product/models.py
+++ product/models.py
@@ -5,3 +5,12 @@ class Product(models.Model):
     name = models.CharField('nome', max_length=20)
     description = models.CharField('description', max_length=200)
     value = models.FloatField('value')
+
+
+    def validate_filds(self):
+        if (self.name != str) and (self.description != str):
+            raise ValueError('Type incorrect')
+        
+        if self.value != float:
+            raise ValueError('Type incorrect')
+        
